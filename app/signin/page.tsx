54587e5c8ee15776ad340fb75006fdd534936ac4
nuux <nuuxdev@gmail.com>
2025-10-15 18:13:23 +0300

    FIX| loading state for sign in

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useAuthActions } from "@convex-dev/auth/react";
+import { useConvexAuth } from "convex/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export default function SignIn() {
   const { signIn } = useAuthActions();
+  const [isLoading, setIsLoading] = useState(false);
   const [flow] = useState<"signIn" | "signUp">("signIn");
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
+
   return (
     <main className="center">
       <h1>
@@ -17,23 +21,26 @@ export default function SignIn() {
       <p className="subheader">the least I can do for emamaye</p>
       <form
         style={{ display: "grid", gap: "1rem" }}
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault();
           const formData = new FormData(e.target as HTMLFormElement);
           formData.set("flow", flow);
-          void signIn("password", formData)
-            .catch((error) => {
-              setError(error.message);
-            })
-            .then(() => {
-              router.push("/");
-            });
+          try {
+            setIsLoading(true);
+            await signIn("password", formData);
+            router.push("/");
+          } catch (error) {
+            setError("Oops!😯 Error signing in!");
+          } finally {
+            setIsLoading(false);
+          }
         }}
       >
         <input type="email" name="email" placeholder="Email" />
         <input type="password" name="password" placeholder="Password" />
         <button type="submit" className="primary-button">
-          {flow === "signIn" ? "Sign in" : "Sign up"}
+          {/* {flow === "signIn" ? "Sign in" : "Sign up"} */}
+          {isLoading ? "Signing In..." : "Sign in"}
         </button>
         {/* <div className="inline-flex">
           <p>
@@ -47,9 +54,7 @@ export default function SignIn() {
         </div> */}
         {error && (
           <div className="bg-red-500/20 border-2 border-red-500/50 rounded-md p-2">
-            <p className="text-foreground font-mono text-xs">
-              Oops!😯Error signing in!
-            </p>
+            <p className="text-foreground font-mono text-xs">{error}</p>
           </div>
         )}
       </form>
